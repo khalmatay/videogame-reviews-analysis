77c90bc1f85dd836b3ca20b9c7f4325a4aea6065
khalmatay <khalmatay@gmail.com>
2024-11-03 11:48:15 +0100

    fix AQ

--- functions.py
+++ functions.py
@@ -1,23 +1,20 @@
-def algo_question() -> None:
-    with open('input.txt', 'r') as file:
+def algo_question(file_name: str) -> str:
+    with open(file_name, 'r') as file:
         t = int(file.readline().strip())
+        result = []
         for _ in range(t):
             n, k = map(int, file.readline().strip().split())
             n_even = n - (k - 1)
             if n_even > 0 and n_even % 2 == 1:
-                print("YES")
-                s = ""
-                for j in range(k-1):
-                    s += " 1"
-                print(str(n_even) + s)
+                result.append("YES")
+                result.append(f"{n_even}" + " 1" * (k - 1))
                 continue
             n_odd = n - 2 * (k - 1)
             if n_odd > 0 and n_odd % 2 == 0:
-                print("YES")
-                s =""
-                for j in range(k-1):
-                    s += " 2"
-                print(str(n_even) + s)
+                result.append("YES")
+                result.append(f"{n_odd}" + " 2" * (k - 1))
                 continue
-            print("NO")
-algo_question()
+            result.append("NO")
+    return "\n".join(result)
+
+
